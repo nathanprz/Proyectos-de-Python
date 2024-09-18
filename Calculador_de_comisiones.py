57e454ff09855f53c3c7b68aa4b9c55cd254f370
Jonathan Uziel Sánchez Oropeza <101217072+jonathanshz@users.noreply.github.com>
2024-09-18 13:55:59 -0600

    Poniendo un espacio xd

--- Calculador_de_comisiones.py
+++ Calculador_de_comisiones.py
@@ -10,5 +10,6 @@ ventas = round(ventas,2)
 
 #Operacion y redondeo
 ganancias = round(ventas * comision / 100,2)
+
 #Muestra las ganancias del usuario y su nombre
 print(f"Hola {nombre}\nComo has vendido {ventas} y tus comiciones son del {comision}\n\tTUS GANANCIAS SON:{ganancias} pesos")
